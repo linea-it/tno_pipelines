302e61a9bca65aab328dd46d844cf13be9e92bd2
glaubervila <glauber.vila.verde@gmail.com>
2025-09-04 15:31:39 +0000

    Adds execution environment variable and refines JSON keys
    Introduces an `EXECUTION_ENV` variable for better environment configuration.
    
    Updates the JSON key from `step` to `ephemeris_step` for clarity.
    
    Removes obsolete log and data files to streamline the project.

--- env.template.sh
+++ env.template.sh
@@ -4,6 +4,9 @@ export PIPELINES_DIR="<PIPELINES_DIR>"
 export PYTHONPATH=$PIPELINES_DIR/predict_occultation/packages
 export PATH=$PATH:$PIPELINES_DIR/predict_occultation/scripts
 
+# Execution Environment local or linea (linea use eups for setup fortrans)
+export EXECUTION_ENV="local"
+
 # Database Connections URI
 export DB_ADMIN_URI="postgresql+psycopg2://postgres:postgres@localhost:5432/postgres"
 export DB_CATALOG_URI="postgresql+psycopg2://postgres:postgres@localhost:5432/prod_gavo"
--- predict_occultation/run.sh
+++ predict_occultation/run.sh
@@ -27,6 +27,33 @@ echo "Installing pipeline..."
 
 set -xe
 
+echo "Environment: "$EXECUTION_ENV
+
+if [[ "$EXECUTION_ENV" = "linea" ]]
+then
+    echo "Setup remote env at LIneA (Slurm)"
+
+    export EUPS_USERDATA=/tmp/`whoami`/eups
+    . /opt/eups/bin/setups.sh
+
+    echo "Eups Setup: gcc 11.1.0+0"
+    setup gcc 11.1.0+0
+
+    echo "Eups Setup: geradata 20240101+0"
+    setup geradata 20240101+0
+
+    echo "Eups Setup: elimina 20240101+0"
+    setup elimina 20240101+0
+
+    echo "Eups Setup: praia_occ_star_search_12 20240101+0"
+    setup praia_occ_star_search_12 20240101+0    
+
+    ulimit -s 100000
+    ulimit -u 100000
+
+    echo "Setup environment at LineA done."
+fi
+
 # Run the Python code with the given argument
 predict-run $ARGS || { echo "Failed to predict-run"; exit 1; }
 
