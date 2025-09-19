314e0144080bc2ca8f6535e87405077da72fa4b8
glaubervila <glauber.vila.verde@gmail.com>
2025-09-19 16:23:05 -0300

    optimized docker image

--- env.template.sh
+++ env.template.sh
@@ -11,9 +11,13 @@ export EXECUTION_ENV="local"
 export DB_ADMIN_URI="postgresql+psycopg2://postgres:postgres@localhost:5432/postgres"
 export DB_CATALOG_URI="postgresql+psycopg2://postgres:postgres@localhost:5432/prod_gavo"
 
-# External Inputs Path
+# Directory for external asteroid inputs (asteroid ephemeris files, bsp, etc)
+export ASTEROID_INPUTS_DIR="/data/asteroids"
+
+# Directory for common inputs (dates.parquet, de440.bsp, naif0012.tls, etc)
+export COMMON_INPUTS_DIR="/data/common_inputs"
 
 # de440.bsp complete filepath
-PLANETARY_EPHEMERIS_FILEPATH="/app/de440.bsp"
+export PLANETARY_EPHEMERIS_FILEPATH="/app/de440.bsp"
 # naif0012.tls complete filepath
-LEAP_SECONDS_FILEPATH="/app/naif0012.tls"
+export LEAP_SECONDS_FILEPATH="/app/naif0012.tls"
